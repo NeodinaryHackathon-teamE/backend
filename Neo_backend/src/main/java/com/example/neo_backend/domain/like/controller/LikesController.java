336ea09fdef227d00af6284cb32fbc33e53ef44c
inha0319@naver.com <inha031913@gmail.com>
2025-05-18 00:48:37 +0900

    [feat] 좋아요 틀 구성

--- Neo_backend/src/main/java/com/example/neo_backend/domain/like/controller/LikesController.java
+++ Neo_backend/src/main/java/com/example/neo_backend/domain/like/controller/LikesController.java
@@ -5,6 +5,7 @@ import com.example.neo_backend.domain.like.dto.LikeResponseDto;
 import com.example.neo_backend.domain.like.service.LikesService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,13 +20,5 @@ import org.springframework.web.bind.annotation.RestController;
 public class LikesController {
 
     private final LikesService likesService;
-
-    @PostMapping
-    @Operation(summary = "좋아요 생성", description = "게시글에 좋아요 또는 좋아요 취소를 요청합니다.")
-    public ResponseEntity<LikeResponseDto> likePost(@RequestBody LikeRequestDto dto) {
-        LikeResponseDto response = likesService.likePost(dto);
-        return ResponseEntity.ok(response);
-    }
-
 
 }
