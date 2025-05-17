a7bbec8f9acc25752d71cd9a1a13269814f256b3
Yoonji Lee <estelle0329@ewha.ac.kr>
2025-05-18 03:21:44 +0900

    docs: errorstatus 수정

--- Neo_backend/src/main/java/com/example/neo_backend/global/common/dto/ErrorReason.java
+++ Neo_backend/src/main/java/com/example/neo_backend/global/common/dto/ErrorReason.java
@@ -1,12 +1,15 @@
 package com.example.neo_backend.global.common.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@AllArgsConstructor
 public class ErrorReason {
     private final Integer status;
     private final String code;
     private final String reason;
+    private final String info;
 }
--- Neo_backend/src/main/java/com/example/neo_backend/global/common/exception/BaseErrorCode.java
+++ Neo_backend/src/main/java/com/example/neo_backend/global/common/exception/BaseErrorCode.java
@@ -3,7 +3,7 @@ package com.example.neo_backend.global.common.exception;
 import com.example.neo_backend.global.common.dto.ErrorReason;
 
 public interface BaseErrorCode {
-    public ErrorReason getErrorReason();
+    ErrorReason getErrorReason();
 
     String getExplainError() throws NoSuchFieldException;
 }
