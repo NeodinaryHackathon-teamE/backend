d0bfc88b4999326aeaa697e3e26559da1dacb7c7
Yoonji Lee <estelle0329@ewha.ac.kr>
2025-05-17 22:37:41 +0900

    feat: 로그인 API 추가

--- Neo_backend/src/main/java/com/example/neo_backend/domain/user/controller/AuthApiController.java
+++ Neo_backend/src/main/java/com/example/neo_backend/domain/user/controller/AuthApiController.java
@@ -2,6 +2,8 @@ package com.example.neo_backend.domain.user.controller;
 
 import com.example.neo_backend.domain.user.dto.AuthDto;
 import com.example.neo_backend.domain.user.service.AuthService;
+import com.example.neo_backend.global.common.response.ApiResponse;
+import com.example.neo_backend.global.common.status.SuccessStatus;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/auth")
+@RequestMapping("/api/v1/auth")
 @Tag(name = "Auth", description = "인증 관련 API")
 public class AuthApiController {
 
@@ -18,8 +20,7 @@ public class AuthApiController {
 
     @PostMapping("/signup")
     @Operation(summary = "회원가입", description = "이메일, 비밀번호, 닉네임으로 회원가입을 진행합니다.")
-    public ResponseEntity<String> signup(@RequestBody AuthDto authDTO) {
+    public ResponseEntity<ApiResponse> signup(@RequestBody AuthDto authDTO) {
         authService.signup(authDTO);
-        return ResponseEntity.ok("회원가입이 완료되었습니다.");
-    }
+        return ApiResponse.onSuccess(SuccessStatus._OK);    }
 }
