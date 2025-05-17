3c3baa6edb7fb934af8bb4e88ea7b5a424af62d6
inha0319@naver.com <inha031913@gmail.com>
2025-05-17 15:52:24 +0900

    chore : 초기 셋팅

---
+++ Neo_backend/src/main/java/com/example/neo_backend/NeoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.neo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoBackendApplication.class, args);
+	}
+
+}
